e8b4b6a35990846221e08ade8baee2206ea512ce
bebe <benish.khan@netlify.com>
2023-05-16 16:07:53 -0700

    Updated title to Boxing Bee

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home({ products }) {
   return (
     <>
       <Head>
-        <title>Cheese and Meat Shop</title>
+        <title>Boxing Bee</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
